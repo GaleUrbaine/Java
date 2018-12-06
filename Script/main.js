f30e9555a0f9a4c091d7d0032f9b5341b8850058
GaleUrbaine <bloody_spyke@hotmail.fr>
2018-12-06 07:54:23 +0100

    ajout commande java

--- Script/main.js
+++ Script/main.js
@@ -1,2 +1,11 @@
-let header = document.querySelector('h1');
-header.testContent = 'Hello world!';
+<html>
+  <body>
+
+    <form name="popup.html">
+      <p>Le Popup aura pour titre : <input type='text' value='xxxvidsxxx' name="titre">
+      <p><input type="setTimeout(function () {
+
+      }, 10);" value="Générer le popup" onclick="popup()">
+</form>
+  </body>
+</html>
